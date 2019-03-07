7e0dca6b52edd0fc64206c9f8109b4165c77ac8b
Brian D. Johnson <brian@thejohnsonfamily.name>
2019-03-06 22:34:01 -0500

    Upgraded to PMS 1.15.1.710, Enhanced build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ if [ "${SCRIPT_DIR}" = "" ] || [ ! -e "${SCRIPT_DIR}" ]; then
 fi
 
 # Download debian 64-bit version from https://www.plex.tv/downloads/
-plex_file=~/Downloads/plexmediaserver_1.14.1.5488-cc260c476_amd64.deb
+plex_file=~/Downloads/plexmediaserver_1.15.1.710-ece95b3a1_amd64.deb
 echo "Plex Download is: ${plex_file}"
 if [ ! -f ${plex_file} ]; then
 	echo "ERROR: Plex download file is missing or has bad permissions: ${plex_file}"
@@ -21,37 +21,43 @@ fi
 version=$(basename "${plex_file}" | sed -e "s|.*_\(.*\)-.*|\\1|")
 echo "Plex Version: ${version}"
 
+# cleanup SCRIPT_DIR/lib/*
+echo -en "Cleaning up: ${SCRIPT_DIR}/lib/*: " && {
+	/bin/rm -rf ${SCRIPT_DIR}/lib/*
+	echo "DONE"
+} || exit 1
+
+# Install
+echo "Preparing ${SCRIPT_DIR}/lib" && pushd ${SCRIPT_DIR}/lib >/dev/null
+echo "Creating tmp/ dir" && mkdir tmp && pushd tmp/ >/dev/null
+
+echo -en "Extracting ${plex_file}: " && {
+	ar x ${plex_file} &>/dev/null &&
+	echo "DONE"
+} || exit 1
+
+echo -en "Extracting data.tar.*z: " && {
+	tar xvfa data.tar.*z &>/dev/null &&
+	echo "DONE"
+} || exit 1
+
+echo -en "Installing Plex Libs to ${SCRIPT_DIR}/lib: " && {
+	/bin/mv usr/lib/plexmediaserver/* ${SCRIPT_DIR}/lib/ &&
+	echo "DONE"
+} || exit 1
+echo "Cleaning up..." && popd > /dev/null && /bin/rm -rf tmp && popd >/dev/null
+
 # Change version in `addon.xml` to match downloaded version
 echo -en "Updating: addon.xml: " && {
 	sed -i "s|^\(       version\)=\".*\"$|\1=\"${version}\"|" ${SCRIPT_DIR}/addon.xml
 	echo "DONE"
-}
+} || exit 1
 
 # Update changelog.txt
 echo -en "Updating: changelog.txt: " && {
 	sed -i "1s/^/${version}\n- update to PMS ${version}\n\n/" ${SCRIPT_DIR}/changelog.txt
 	echo "DONE"
-}
-
-# cleanup SCRIPT_DIR/lib/*
-echo -en "Cleaning up: ${SCRIPT_DIR}/lib/*: " && {
-	/bin/rm -rf ${SCRIPT_DIR}/lib/*
-	echo "DONE"
-}
-
-# Install
-echo "Preparing ${SCRIPT_DIR}/lib" && pushd ${SCRIPT_DIR}/lib >/dev/null
-echo "Creating tmp/ dir" && mkdir tmp && pushd tmp/ >/dev/null
-echo -en "Extracting ${plex_file}: " && {
-	ar x ${plex_file} &>/dev/null
-	echo "DONE"
-}
-echo -en "Extracting data.tar.gz: " && {
-	tar xvfx data.tar.gz &>/dev/null
-	echo "DONE"
-}
-echo "Installing Plex Libs to ${SCRIPT_DIR}/lib" && /bin/mv usr/lib/plexmediaserver/* ${SCRIPT_DIR}/lib/
-echo "Cleaning up..." && popd > /dev/null && /bin/rm -rf tmp && popd >/dev/null
+} || exit 1
 
 # Package
 echo "Packaging..."
