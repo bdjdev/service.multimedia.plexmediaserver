1493c04a98dd5582a50042d99fbc2e440ea7951a
Brian D. Johnson <bdjdev@users.noreply.github.com>
2020-10-25 14:28:12 -0400

    Moved add source files to src subdir

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 declare -r SCRIPT=${0##*/}
 declare -r SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+declare -r SRC_DIR="${SCRIPT_DIR}/src"
+declare -r ADDON_DIR="${SRC_DIR}/service.multimedia.plexmediaserver"
+declare -r OUTPUT_DIR="${SCRIPT_DIR}/output"
 
 # validate SCRIPT_DIR
 if [ "${SCRIPT_DIR}" = "" ] || [ ! -e "${SCRIPT_DIR}" ]; then
@@ -28,7 +31,7 @@ fi
 version=$(basename "${plex_file}" | sed -e "s|.*_\(.*\)-.*|\\1|")
 echo "Plex Version: ${version}"
 
-output_file=service.multimedia.plexmediaserver-${version}.zip
+output_file="${OUTPUT_DIR}/service.multimedia.plexmediaserver-${version}.zip"
 if [ -f ${output_file} ]; then
 	echo "This will overwrite the existing file: ${output_file}"
 	unset input
@@ -53,14 +56,14 @@ if [ -f ${output_file} ]; then
 	done
 fi
 
-# cleanup SCRIPT_DIR/lib/*
-echo -en "Cleaning up: ${SCRIPT_DIR}/lib/*: " && {
-	/bin/rm -rf ${SCRIPT_DIR}/lib/*
+# cleanup ADDON_DIR/lib
+echo -en "Cleaning up: ${ADDON_DIR}/lib: " && {
+	/bin/rm -rf ${ADDON_DIR}/lib
 	echo "DONE"
 } || exit 1
 
 # Install
-echo "Preparing ${SCRIPT_DIR}/lib" && pushd ${SCRIPT_DIR}/lib >/dev/null
+echo "Preparing ${ADDON_DIR}/lib" && mkdir -p ${ADDON_DIR}/lib && pushd ${ADDON_DIR}/lib >/dev/null
 echo "Creating tmp/ dir" && mkdir tmp && pushd tmp/ >/dev/null
 
 echo -en "Extracting ${plex_file}: " && {
@@ -73,33 +76,33 @@ echo -en "Extracting data.tar.*z: " && {
 	echo "DONE"
 } || exit 1
 
-echo -en "Installing Plex Libs to ${SCRIPT_DIR}/lib: " && {
-	/bin/mv usr/lib/plexmediaserver/* ${SCRIPT_DIR}/lib/ &&
+echo -en "Installing Plex Libs to ${ADDON_DIR}/lib: " && {
+	/bin/mv usr/lib/plexmediaserver/* ${ADDON_DIR}/lib/ &&
 	echo "DONE"
 } || exit 1
 echo "Cleaning up..." && popd > /dev/null && /bin/rm -rf ./tmp && popd >/dev/null
 
 # Change version in 'addon.xml' to match downloaded version
 echo -en "Updating: addon.xml: " && {
-	sed -i "s|^\(       version\)=\".*\"$|\1=\"${version}\"|" ${SCRIPT_DIR}/addon.xml
+	sed -i "s|^\(       version\)=\".*\"$|\1=\"${version}\"|" ${ADDON_DIR}/addon.xml
 	echo "DONE"
 } || exit 1
 
 # Update changelog.txt
 echo -en "Updating: changelog.txt: " && {
-	if grep "^${version}$" ${SCRIPT_DIR}/changelog.txt &>/dev/null; then
+	if grep "^${version}$" ${ADDON_DIR}/changelog.txt &>/dev/null; then
 		echo "SKIPPING (Already Updated)"
 	else
-		sed -i "1s/^/${version}\n- update to PMS ${version}\n\n/" ${SCRIPT_DIR}/changelog.txt
+		sed -i "1s/^/${version}\n- update to PMS ${version}\n\n/" ${ADDON_DIR}/changelog.txt
 		echo "DONE"
 	fi
 } || exit 1
 
 # Package
 echo "Packaging..."
-cd ${SCRIPT_DIR}/..
+cd ${SRC_DIR}
 echo -en "Creating: ${output_file}: " && {
-	zip -r ${output_file} service.multimedia.plexmediaserver/ -x *.git* *build.sh *Readme.md */output/* &>/dev/null
+	zip -r ${output_file} $(basename ${ADDON_DIR})/ -x *.git*  &>/dev/null
 	echo "DONE"
 }
 
